9ce4dfe2cddfe49bf8bd5a3f709f17598baa8494
x-state-7 <chaudharyhemantkumar030@gmail.com>
2025-02-24 11:42:07 +0530

    creating jsx for weather widget

--- news-blogs-app/src/Components/Weather.jsx
+++ news-blogs-app/src/Components/Weather.jsx
@@ -4,7 +4,25 @@ import './Weather.css'
 const Weather = () => {
   return (
     <div className="weather">
-      Weather
+
+      <div className="search">
+        <div className="search-top">
+          <i className="fa-solid fa-location-dot"></i>
+          <div className="location">Delhi</div>
+        </div>
+
+        <div className="search-location">
+          <input type="text" placeholder="Enter Location" />
+          <i className="fa-solid fa-magnifying-glass"></i>
+        </div>
+      </div>
+      {/* weather data*/}
+      <div className="weather-data">
+        <i className='bx bxs-sun'></i>
+        <div className="weather-type">Clear</div>
+        <div className="temp">67c</div>
+
+      </div>
     </div>
   )
 }
